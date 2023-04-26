c9da5b6e3d2ef491f14138d0a2a176854dfe21c3
Ryan-Post9 <124849297+Ryan-Post9@users.noreply.github.com>
2023-04-26 10:42:34 -0400

    ecs

---
+++ GameManager/6. World Manager/Levels/level2.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace ECS_Framework
+{
+    /// <summary>
+    /// Implements the <see cref="LevelInitializer"/> interface to provide initialization logic for Level 2.
+    /// </summary>
+    public class Level2Initializer : LevelInitializer
+    {
+        /// <summary>
+        /// Returns a list of entities to be created and added to the level.
+        /// </summary>
+        /// <returns>A list of entities.</returns>
+        public List<Entity> GetObjects()
+        {
+            List<Entity> objects = new List<Entity>();
+            //objects.Add(EntityFactory.CreateParallaxBackground("bg_yellow", new Vector2(0, 50)));
+            objects.Add(EntityFactory.CreateApple(new Vector2(550, 30)));
+            objects.Add(EntityFactory.CreatePlayer(new Vector2(50, 20)));
+            return objects;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# ECS-MonoGame
+<br/>Implementation of Simple ECS Framework using MonoGame
+<br/>Assets Used: https://pixelfrog-assets.itch.io/pixel-adventure-1
+<br/>Documentation: https://dreamystranger.github.io/ECS-MonoGame/annotated.html
